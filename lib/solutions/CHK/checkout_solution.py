b62b9794913ca483806db57dee10ec039dc1fe16
sbx_user1051 <sbx_user1051@169.254.109.101>
2024-07-04 13:20:49 +0000

    Thu Jul 04 13:20:49 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from dataclasses import dataclass
 
 _SKU_PRICE = {
     'A': 50,
@@ -8,17 +7,45 @@ _SKU_PRICE = {
     'D': 15,
     'E': 40,
     'F': 10,
+    'G': 20,
+    'H': 10,
+    'I': 35,
+    'J': 60,
+    'K': 80,
+    'L': 90,
+    'M': 15,
+    'N': 40,
+    'O': 10,
+    'P': 50,
+    'Q': 30,
+    'R': 50,
+    'S': 30,
+    'T': 20,
+    'U': 40,
+    'V': 50,
+    'W': 20,
+    'X': 90,
+    'Y': 10,
+    'Z': 50,
 }
 
 
 _VALUE_OFFERS = {
     'A': {3: 130, 5: 200},
     'B': {2: 45},
+    'H': {5: 45, 10: 80},
+    'K': {2: 150},
+    'P': {5: 200},
+    'Q': {3: 80},
+    'V': {2: 90, 3: 130}
 }
 
 _FREE_ITEM_OFFERS = {
     'E': {2: 'B'},
-    'F': {3: 'F'}
+    'F': {3: 'F'},
+    'N': {3: 'M'},
+    'R': {3: 'Q'},
+    'U': {4: 'U'}
 }
 
 # noinspection PyUnusedLocal
@@ -73,6 +100,7 @@ def _handle_free_items(basket, sku):
             if offers[vol] in basket:
                 full_deals = basket[sku] // vol
                 basket[offers[vol]] -= full_deals
+
 
 
 
