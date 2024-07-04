7c555fe59683502cfe7d8d103505153c7ab2446a
sbx_user1051 <sbx_user1051@169.254.109.101>
2024-07-04 10:46:57 +0000

    Thu Jul 04 10:46:57 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,33 @@
-
-
+_SKU_PRICE_TABLE = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15
+}
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    Accepts a string of all SKUs for a basket and returns the basket value.
+    Returns -1 if any invalid SKUs are passed in.
+
+    Params
+    ------
+    skus: string
+
+    Returns
+    -------
+    int
+    """
+    # Need to know what skus looks like
+
+
+    for sku in skus:
+        if sku not in _SKU_PRICE_TABLE:
+            return -1
+
+        # TODO add condition for special offers
+
+
