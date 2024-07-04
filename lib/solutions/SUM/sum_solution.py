beb764037288f1d407b373ac14400d0fae104a40
sbx_user1051 <sbx_user1051@169.254.109.101>
2024-07-04 09:58:55 +0000

    Thu Jul 04 09:58:55 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,20 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    """
+    Sums two values
+
+    Params
+    ------
+    x: int
+    y: int
+
+    Returns
+    -------
+    int
+    """
+    # Want to add validation but will see what happens if I do nothing first
+
+    return x + y
+
+
+
