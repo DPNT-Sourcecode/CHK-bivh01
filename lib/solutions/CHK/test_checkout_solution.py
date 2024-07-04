f3a40d080dee857e4dd6d07b6b2e4ffdb3fe066c
sbx_user1051 <sbx_user1051@169.254.109.101>
2024-07-04 12:38:21 +0000

    Thu Jul 04 12:38:21 UTC 2024

--- lib/solutions/CHK/test_checkout_solution.py
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -6,22 +6,25 @@ class TestCheckout:
     @pytest.mark.parametrize(
         "skus, value",
         [
-            ("", 0),
-            ("A", 50),
-            ("B", 30),
-            ("C", 20),
-            ("D", 15),
-            ("-", -1),
-            ("AAA", 130),
-            ("BB", 45),
-            ("BBB", 75),
-            ('E', 40),
-            ('EEB', 80),
-            ("AAAAA", 200),
-            ("AAAAAAAA", 330),
+            # ("", 0),
+            # ("A", 50),
+            # ("B", 30),
+            # ("C", 20),
+            # ("D", 15),
+            # ("-", -1),
+            # ("AAA", 130),
+            # ("BB", 45),
+            # ("BBB", 75),
+            # ('E', 40),
+            # ('EEB', 80),
+            # ("AAAAA", 200),
+            # ("AAAAAAAA", 330),
+            ("BEBEEE", 160),
+            ("ABCDEABCDE", 280),
             ("ABCDCBAABCABBAAA", 495)
         ]
     )
     def test_expected_output(self, skus, value):
         assert value == checkout(skus)
+
 
